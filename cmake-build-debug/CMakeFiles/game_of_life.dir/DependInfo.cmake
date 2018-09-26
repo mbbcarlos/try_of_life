75df96d080d16f9115dab75568eb0103ee88262f
Marta Carlos <m.carlos@campus.fct.unl.pt>
2018-09-26 11:56:19 +0100

    Initial commit
    No changes were made to the original class assignment. This is just an initial commit to possibly fall back into.

---
+++ cmake-build-debug/CMakeFiles/game_of_life.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/martacarlos/Documents/Faculdade/4º ano (2018-2019)/1º Semestre/CP/Aulas/Práticas/Prática 2/try_of_life/config.c" "/Users/martacarlos/Documents/Faculdade/4º ano (2018-2019)/1º Semestre/CP/Aulas/Práticas/Prática 2/try_of_life/cmake-build-debug/CMakeFiles/game_of_life.dir/config.c.o"
+  "/Users/martacarlos/Documents/Faculdade/4º ano (2018-2019)/1º Semestre/CP/Aulas/Práticas/Prática 2/try_of_life/game.c" "/Users/martacarlos/Documents/Faculdade/4º ano (2018-2019)/1º Semestre/CP/Aulas/Práticas/Prática 2/try_of_life/cmake-build-debug/CMakeFiles/game_of_life.dir/game.c.o"
+  "/Users/martacarlos/Documents/Faculdade/4º ano (2018-2019)/1º Semestre/CP/Aulas/Práticas/Prática 2/try_of_life/main.c" "/Users/martacarlos/Documents/Faculdade/4º ano (2018-2019)/1º Semestre/CP/Aulas/Práticas/Prática 2/try_of_life/cmake-build-debug/CMakeFiles/game_of_life.dir/main.c.o"
+  "/Users/martacarlos/Documents/Faculdade/4º ano (2018-2019)/1º Semestre/CP/Aulas/Práticas/Prática 2/try_of_life/mem.c" "/Users/martacarlos/Documents/Faculdade/4º ano (2018-2019)/1º Semestre/CP/Aulas/Práticas/Prática 2/try_of_life/cmake-build-debug/CMakeFiles/game_of_life.dir/mem.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/Users/martacarlos/Documents/Faculdade/4º ano (2018-2019)/1º Semestre/CP/Aulas/Práticas/Prática 2/try_of_life/."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
